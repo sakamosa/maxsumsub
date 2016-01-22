37ef6e266c30d52198687209bb13f86a8aad6e39
buckinja <buckinja@onid.oregonstate.edu>
2016-01-21 20:35:03 -0800

    added comments

--- mainMaxsub.py
+++ mainMaxsub.py
@@ -21,22 +21,31 @@ the 'number' variable indicates how many times you want the function to run
 for time tests
 """
 
+# open files for reading/writing
 in_file = open('MSS_Problems.txt', 'r')
 out_file = open('MSS_Results.txt', 'w')
 
+#infinite looping ... 
 while (1):
+	# read a line from file
 	line = in_file.readline()
+	# if EOF or if line doesn't contain an array (second condition necessary on MSS_Problems.txt), break
 	if (not line) or (len(line) < 2):
 		break
+	# line is assigned an array created from the integers in line, removing brackets, whitespace, and delimiting commas
 	line = [int(i) for i in line.replace("[","").replace(" ","").replace("]","").split(",")]
-#	if len(line) != 0
+
 	print "Algorithm 1"
+	# timeit clocking function time
 	t = Timer(lambda: maxsub.maxSubarray1(line, results))
+	# if we remove this print statement we will need to make sure we still run t.timeit for clocking and to get result
 	print "Running time: " + str(t.timeit(number=1))
+	# write result as string to file
 	out_file.write(str(results))
 	out_file.write('\n')
 
 out_file.write('\n')
+# rewind to beginning of file and do it again with algorithm 2...
 in_file.seek(0)
 while (1):
 	line = in_file.readline()
@@ -50,6 +59,7 @@ while (1):
 	out_file.write('\n')
 
 out_file.write('\n')
+# rewind to beginning of file and do it again with algorithm 3...
 in_file.seek(0)
 while (1):
 	line = in_file.readline()
@@ -63,6 +73,7 @@ while (1):
 	out_file.write('\n')
 
 out_file.write('\n')
+# rewind to beginning of file and do it again with algorithm 4...
 in_file.seek(0)
 while (1):
 	line = in_file.readline()
@@ -75,6 +86,6 @@ while (1):
 	out_file.write(str(results))
 	out_file.write('\n')
 
-
+# close files
 out_file.close()
 in_file.close()
